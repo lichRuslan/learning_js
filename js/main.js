0aca63b46a7c1092d4ae2fcd959cd6855104bbf5
lichruslan <lichruslan@github.com>
2019-01-07 18:46:26 +0600

    Строковые опции

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-var arr = new Array(95,8,7,6,5,4,3,2,1);
-let qwe;
-for (let i = 0; i < arr.length; i++) {
-    qwe +=arr[i];
-}
-text = "Длинна массива = " + arr.length;
-text += "\n Массив состоит из : [" + arr.join(", ")+ "]";
-text += "\n Сортированный массив :["+ (arr.sort()).join(', ')+ "]";
+var str = "String asdasdasd";
+var text = "Длин строки " + str.length;
+text+="\n Строка в вверхнем регистре - " + str.toUpperCase();
+text+="\n страка в нижнем регистре - " + str.toLowerCase();
+text+="\n Поиск 4 символа  - " + str.charAt(4);
+
+text+="\n asd начинается с  индекса - " + str.indexOf('asd');
+
+
+
 alert(text);
-alert(qwe);
