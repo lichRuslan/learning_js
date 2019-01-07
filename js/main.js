05d78912e4ccdec93e54b058d8705f71b11d79cf
lichruslan <lichruslan@github.com>
2019-01-07 16:07:37 +0600

    Массивы

--- js/main.js
+++ js/main.js
@@ -1,38 +1,10 @@
-let date = new Date();
-date.setMonth(+1);
-let text = "Год - " + date.getFullYear() + "<br> Месяц - " + date.getMonth() + '<br> День - ' + date.getDate() + '<br>Время - ' + date.getHours() + ':'+ date.getMinutes();
-document.write(text);
-
-let text2 = "";
-switch (date.getMonth()) {
-    case 1: text2 = "Январь";
-        break;
-    case 2: text2 = "Февраль";
-        break;
-    case 3: text2 = "Март";
-        break;
-    case 4: text2 = "Апрель";
-        break;
-    case 5: text2 = "Май";
-        break;
-    case 6: text2 = "Июнь";
-        break;
-    case 7: text2 = "Июль";
-        break;
-    case 8: text2 = "Август";
-        break;
-    case 9: text2 = "Сеньтябрь";
-        break;
-    case 10: text2 = "Октябрь";
-        break;
-    case 11: text2 = "Ноябрь";
-        break;
-    case 12: text2 = "Декабрь";
-        break;
-    default:
-        text2 = "Ебать капать теперь у нас больше месяцов в году!";
-        break;
+var arr = new Array(95,8,7,6,5,4,3,2,1);
+let qwe;
+for (let i = 0; i < arr.length; i++) {
+    qwe +=arr[i];
 }
-
-
-document.write("<br>"+text2);
+text = "Длинна массива = " + arr.length;
+text += "\n Массив состоит из : [" + arr.join(", ")+ "]";
+text += "\n Сортированный массив :["+ (arr.sort()).join(', ')+ "]";
+alert(text);
+alert(qwe);
