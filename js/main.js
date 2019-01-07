62ea9535343a4ed4820162603eaa4e3ded076b59
lichruslan <lichruslan@github.com>
2019-01-07 15:53:00 +0600

    Date

--- js/main.js
+++ js/main.js
@@ -1,13 +1,38 @@
-// var id = setInterval('myFunct_1()', 1000);
-var counter = 0;
+let date = new Date();
+date.setMonth(+1);
+let text = "Год - " + date.getFullYear() + "<br> Месяц - " + date.getMonth() + '<br> День - ' + date.getDate() + '<br>Время - ' + date.getHours() + ':'+ date.getMinutes();
+document.write(text);
 
-function myFunct_1(params) {
-    // counter++;
-    document.write(counter++);
+let text2 = "";
+switch (date.getMonth()) {
+    case 1: text2 = "Январь";
+        break;
+    case 2: text2 = "Февраль";
+        break;
+    case 3: text2 = "Март";
+        break;
+    case 4: text2 = "Апрель";
+        break;
+    case 5: text2 = "Май";
+        break;
+    case 6: text2 = "Июнь";
+        break;
+    case 7: text2 = "Июль";
+        break;
+    case 8: text2 = "Август";
+        break;
+    case 9: text2 = "Сеньтябрь";
+        break;
+    case 10: text2 = "Октябрь";
+        break;
+    case 11: text2 = "Ноябрь";
+        break;
+    case 12: text2 = "Декабрь";
+        break;
+    default:
+        text2 = "Ебать капать теперь у нас больше месяцов в году!";
+        break;
 }
 
-function timer() {
-    counter++;
-    document.getElementById('count').innerHTML= counter;
-    setTimeout('timer()', 1000);
-}
+
+document.write("<br>"+text2);
